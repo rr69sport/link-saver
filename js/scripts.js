2cf92db6adc53cfceb0fcc00978d0246bcd4b1b4
rr69sport <richardramsua@gmail.com>
2021-10-24 00:38:56 -0300

    Use the new functions added in the before commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
-import { createElements } from "./modules/handle_elements.js";
+import { printElements } from "./modules/handle_elements.js";
 
-import { addItem } from "./modules/handle_localstorage.js";
+import { addToLocalStorage, clearLocalStorage } from "./modules/handle_localstorage.js";
 
 const formApp = document.getElementById('form-app');
-const savedContent = document.getElementById('saved-content');
+const listOfLinks = document.getElementById('list-of-links');
+const clearAll = document.getElementById('clear-all');
 
 formApp.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const data = {
         name: formApp.title.value.trim(),
         link: formApp.link.value.trim()
-    }
+    };
+
     if (data.name !== "" && data.link !== "") {
-        addItem(data);
-        savedContent.textContent = "";
-        savedContent.innerHTML = createElements();
+        addToLocalStorage(data);
+        printElements(listOfLinks);
         formApp.reset();
-    }
+    };
+});
+
+clearAll.addEventListener('click', () => {
+    clearLocalStorage();
+    printElements(listOfLinks);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    savedContent.textContent = "";
-    savedContent.innerHTML = createElements();
+    printElements(listOfLinks);
 });
