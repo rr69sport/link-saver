01255479c53263ccc3f201a27044473115396a0d
mrLuisFer <mrluisfeer@gmail.com>
2021-10-29 23:54:43 -0500

    change 15,000 to 1,500 in timeOut

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@ import { nanoid as generateId } from "https://cdn.jsdelivr.net/npm/nanoid/nanoid
 import { printElements } from "./modules/handle_elements.js";
 import { validateUrl } from "./utils/validate_url.js";
 
-import { addToLocalStorage, removeFromLocalStorage, clearLocalStorage } from "./modules/handle_localstorage.js";
+import {
+  addToLocalStorage,
+  removeFromLocalStorage,
+  clearLocalStorage,
+} from "./modules/handle_localstorage.js";
 
 const formApp = document.getElementById("form-app");
 const listOfLinks = document.getElementById("list-of-links");
@@ -12,49 +16,44 @@ const errorMessage = document.getElementById("error-message");
 const inputLink = document.getElementById("link-to-save");
 
 formApp.addEventListener("submit", (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	const data = {
-		id: generateId(),
-		text: formApp.title.value.trim(),
-		link: formApp.link.value.trim(),
-	};
+  const data = {
+    id: generateId(),
+    text: formApp.title.value.trim(),
+    link: formApp.link.value.trim(),
+  };
 
-	const isValidUrl = validateUrl(data.link);
+  const isValidUrl = validateUrl(data.link);
 
-	if (data.text !== "" && isValidUrl) {
+  if (data.text !== "" && isValidUrl) {
+    addToLocalStorage(data);
+    printElements(listOfLinks);
+    formApp.reset();
+  } else {
+    errorMessage.classList.add("error-message--show");
+    inputLink.disabled = true;
 
-		addToLocalStorage(data);
-		printElements(listOfLinks);
-		formApp.reset();
-
-	} else {
-
-		errorMessage.classList.add("error-message--show");
-		inputLink.disabled = true;
-
-		setTimeout(() => {
-
-			errorMessage.classList.remove("error-message--show");
-			inputLink.disabled = false;
-
-		}, 15000);
-	}
+    setTimeout(() => {
+      errorMessage.classList.remove("error-message--show");
+      inputLink.disabled = false;
+    }, 1500);
+  }
 });
 
 clearAll.addEventListener("click", () => {
-	clearLocalStorage();
-	printElements(listOfLinks);
+  clearLocalStorage();
+  printElements(listOfLinks);
 });
 
 listOfLinks.addEventListener("click", (e) => {
-	if (e.target.classList.contains("delete-js")) {
-		const id = e.target.dataset.id;
-		removeFromLocalStorage(id);
-		printElements(listOfLinks);
-	}
+  if (e.target.classList.contains("delete-js")) {
+    const id = e.target.dataset.id;
+    removeFromLocalStorage(id);
+    printElements(listOfLinks);
+  }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-	printElements(listOfLinks);
+  printElements(listOfLinks);
 });
